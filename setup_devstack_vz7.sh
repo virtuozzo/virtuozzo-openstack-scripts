6bad33f6616e249dc49a9fcad1ab375bdaebeb8e
Maxim Nestratov <mnestratov@virtuozzo.com>
2016-11-29 14:31:49 +0300

    update review 397166

--- setup_devstack_vz7.sh
+++ setup_devstack_vz7.sh
@@ -309,7 +309,7 @@ _EOF
 set -x
 fi
 
-clone_and_fix_openstack_project nova refs/changes/14/214314/6,refs/changes/05/355805/7,refs/changes/29/397829/4,refs/changes/88/312488/12,refs/changes/18/396218/2,refs/changes/66/397166/4
+clone_and_fix_openstack_project nova refs/changes/14/214314/6,refs/changes/05/355805/7,refs/changes/29/397829/4,refs/changes/88/312488/12,refs/changes/18/396218/2,refs/changes/66/397166/5
 clone_and_fix_openstack_project glance refs/changes/33/341633/6,refs/changes/23/341623/3
 clone_and_fix_openstack_project cinder refs/changes/35/400235/1,refs/changes/79/357679/9
 fixup_configs_for_libvirt
